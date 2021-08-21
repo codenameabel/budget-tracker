de72f2e39143ec701a99e516f1466ccc80b71366
codenameabel <newabelmartinez@gmail.com>
2021-08-21 11:29:56 -0700

    fixing heroku deployment problem after mongo i

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const url = `mongodb+srv://Abel_Martinez123:eRFpi9PrUlhJSgfO@cluster0.bmf2y.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const url = `mongodb+srv://Abel_Martinez123:eRFpi9PrUlhJSgfO>@cluster0.bmf2y.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 const connectionParams={
     useNewUrlParser: true,
