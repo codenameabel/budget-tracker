16b051376b73922380c11728f32e84829850f22d
codenameabel <newabelmartinez@gmail.com>
2021-08-21 11:10:36 -0700

    mongo added

---
+++ db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+const url = `mongodb+srv://Abel_Martinez123:eRFpi9PrUlhJSgfO@cluster0.bmf2y.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const connectionParams={
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true 
+}
+mongoose.connect(url,connectionParams)
+    .then( () => {
+        console.log('Connected to database ')
+    })
+    .catch( (err) => {
+        console.error(`Error connecting to the database. \n${err}`);
+    })
